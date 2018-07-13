216c41db81b733dd92844690663de06a578681a4
Thijs van Ede <thijsvanede@hotmail.com>
2018-07-13 16:21:16 +0200

    Setup added

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "Licence :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent")
     )
