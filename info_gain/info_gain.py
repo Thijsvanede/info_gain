7eadc1ee9c09b8972a3deba8012d51b4c528fbf6
Thijs van Ede <thijsvanede@hotmail.com>
2018-07-13 16:21:09 +0200

    Replaced tabs by spaces

--- info_gain/info_gain.py
+++ info_gain/info_gain.py
@@ -3,104 +3,104 @@ from scipy.stats import entropy
 import math
 
 def info_gain(Ex, a):
-	""" Compute the information gain of an attribute a for given examples.
-	
-		Parameters
-		----------
-		Ex : list of hashable
-			A list of hashable objects (examples)
-			corresponding to the given attributes a.
-			I.e. a[i] <--> Ex[i].
-		
-		a : list of hashable
-			A list of hashable objects (attributes)
-			corresponding to the given examples Ex.
-			I.e. a[i] <--> Ex[i].
-			
-		Returns
-		-------
-		result : float
-			Information gain by knowing given attributes.
-		
-		"""
-	# Check whether examples and attributes have the same lengths.
-	if len(Ex) != len(a):
-		raise ValueError("Ex and a must be of the same size.")
-	
-	# Compute the entropy of examples
-	H_Ex = entropy(list(Counter(Ex).values()))
-	
-	# Compute the sum of all values v in a
-	sum_v = 0
-	for v in set(a):
-		Ex_a_v = [x for x, t in zip(Ex, a) if t == v]
-		sum_v += (len(Ex_a_v) / len(Ex)) *\
-				 (entropy(list(Counter(Ex_a_v).values())))
-	
-	# Return result
-	return H_Ex - sum_v
-	
-	
+    """ Compute the information gain of an attribute a for given examples.
+
+        Parameters
+        ----------
+        Ex : list of hashable
+            A list of hashable objects (examples)
+            corresponding to the given attributes a.
+            I.e. a[i] <--> Ex[i].
+
+        a : list of hashable
+            A list of hashable objects (attributes)
+            corresponding to the given examples Ex.
+            I.e. a[i] <--> Ex[i].
+
+        Returns
+        -------
+        result : float
+            Information gain by knowing given attributes.
+
+        """
+    # Check whether examples and attributes have the same lengths.
+    if len(Ex) != len(a):
+        raise ValueError("Ex and a must be of the same size.")
+
+    # Compute the entropy of examples
+    H_Ex = entropy(list(Counter(Ex).values()))
+
+    # Compute the sum of all values v in a
+    sum_v = 0
+    for v in set(a):
+        Ex_a_v = [x for x, t in zip(Ex, a) if t == v]
+        sum_v += (len(Ex_a_v) / len(Ex)) *\
+                 (entropy(list(Counter(Ex_a_v).values())))
+
+    # Return result
+    return H_Ex - sum_v
+
+
 def intrinsic_value(Ex, a):
-	""" Compute the intrinsic value of an attribute a for given examples.
-	
-		Parameters
-		----------
-		Ex : list of hashable
-			A list of hashable objects (examples)
-			corresponding to the given attributes a.
-			I.e. a[i] <--> Ex[i].
-		
-		a : list of hashable
-			A list of hashable objects (attributes)
-			corresponding to the given examples Ex.
-			I.e. a[i] <--> Ex[i].
-			
-		Returns
-		-------
-		result : float
-			Intrinsic value of attribute a for samples Ex.
-		
-		"""
-	# Check whether examples and attributes have the same lengths.
-	if len(Ex) != len(a):
-		raise ValueError("Ex and a must be of the same size.")
-	
-	# Compute the sum of all values v in a
-	sum_v = 0
-	for v in set(a):
-		Ex_a_v = [x for x, t in zip(Ex, a) if t == v]
-		sum_v += (len(Ex_a_v) / len(Ex)) * math.log(len(Ex_a_v) / len(Ex), 2)
-	
-	# Return result
-	return -sum_v
-	
-	
+    """ Compute the intrinsic value of an attribute a for given examples.
+
+        Parameters
+        ----------
+        Ex : list of hashable
+            A list of hashable objects (examples)
+            corresponding to the given attributes a.
+            I.e. a[i] <--> Ex[i].
+
+        a : list of hashable
+            A list of hashable objects (attributes)
+            corresponding to the given examples Ex.
+            I.e. a[i] <--> Ex[i].
+
+        Returns
+        -------
+        result : float
+            Intrinsic value of attribute a for samples Ex.
+
+        """
+    # Check whether examples and attributes have the same lengths.
+    if len(Ex) != len(a):
+        raise ValueError("Ex and a must be of the same size.")
+
+    # Compute the sum of all values v in a
+    sum_v = 0
+    for v in set(a):
+        Ex_a_v = [x for x, t in zip(Ex, a) if t == v]
+        sum_v += (len(Ex_a_v) / len(Ex)) * math.log(len(Ex_a_v) / len(Ex), 2)
+
+    # Return result
+    return -sum_v
+
+
 def info_gain_ratio(Ex, a):
-	""" Compute the information gain ratio of an attribute a for given examples.
-	
-		Parameters
-		----------
-		Ex : list of hashable
-			A list of hashable objects (examples)
-			corresponding to the given attributes a.
-			I.e. a[i] <--> Ex[i].
-		
-		a : list of hashable
-			A list of hashable objects (attributes)
-			corresponding to the given examples Ex.
-			I.e. a[i] <--> Ex[i].
-			
-		Returns
-		-------
-		result : float
-			Information gain ratio by knowing given attributes.
-			I.e. information gain normalised with intrinsic value calculation.
-		
-		"""
-	# Check whether examples and attributes have the same lengths.
-	if len(Ex) != len(a):
-		raise ValueError("Ex and a must be of the same size.")
-	
-	# Compute information gain ratio as IG/IV
-	return info_gain(Ex, a) / intrinsic_value(Ex, a)
+    """ Compute the information gain ratio of an attribute a for given examples.
+
+        Parameters
+        ----------
+        Ex : list of hashable
+            A list of hashable objects (examples)
+            corresponding to the given attributes a.
+            I.e. a[i] <--> Ex[i].
+
+        a : list of hashable
+            A list of hashable objects (attributes)
+            corresponding to the given examples Ex.
+            I.e. a[i] <--> Ex[i].
+
+        Returns
+        -------
+        result : float
+            Information gain ratio by knowing given attributes.
+            I.e. information gain normalised with intrinsic value calculation.
+
+        """
+    # Check whether examples and attributes have the same lengths.
+    if len(Ex) != len(a):
+        raise ValueError("Ex and a must be of the same size.")
+
+    # Compute information gain ratio as IG/IV
+    return info_gain(Ex, a) / intrinsic_value(Ex, a)
