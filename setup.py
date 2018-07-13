3096d25d653d200ce0d2d578a797e57ad8e77b94
Thijs van Ede <thijsvanede@hotmail.com>
2018-07-13 15:13:25 +0200

    Installation added - untested

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='info_gain',
+    version='1.0',
+    author='Thijs van Ede',
+    author_email='t.s.vanede@utwente.nl',
+    description='Information gain utilities',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Thijsvanede/info_gain',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Licence :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent")
+    )
