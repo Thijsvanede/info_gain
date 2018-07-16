2d98c18904e0b301d61f82ad74a9bf7301421205
Thijs van Ede <thijsvanede@hotmail.com>
2018-07-16 13:25:46 +0200

    Debugging Nan change

--- info_gain/info_gain.py
+++ info_gain/info_gain.py
@@ -2,44 +2,6 @@ from collections import Counter
 from scipy.stats import entropy
 import math
 
-def _Ex_a_v_(Ex, a, v, nan=True):
-    """ Compute the Ex_a_v value given whether nan==nan or nan!=nan.
-    
-        Parameters
-        ----------
-        Ex : list of hashable
-            A list of hashable objects (examples)
-            corresponding to the given attributes a.
-            I.e. a[i] <--> Ex[i].
-
-        a : list of hashable
-            A list of hashable objects (attributes)
-            corresponding to the given examples Ex.
-            I.e. a[i] <--> Ex[i].
-            
-        v : hashable object
-            The hashable object for which to compute
-            the Ex_a_v value.
-            
-        nan : boolean, default=True
-            Boolean indicating how nan==nan should be evaluated.
-            Default == True to avoid division by 0 errors.
-            
-        Returns
-        -------
-        result : list of hashable.
-            List of Ex where value(a) == v.
-            
-        """
-    if nan:
-        return [x for x, t in zip(Ex, a) if (isinstance(t, float) and
-                                             isinstance(v, float) and
-                                             math.isnan(t)        and
-                                             math.isnan(v))       or
-                                             (t == v)]
-    else:
-        return [x for x, t in zip(Ex, a) if t == v]
-
 
 def info_gain(Ex, a, nan=True):
     """ Compute the information gain of an attribute a for given examples.
@@ -73,14 +35,14 @@ def info_gain(Ex, a, nan=True):
     # Compute the entropy of examples
     H_Ex = entropy(list(Counter(Ex).values()))
 
-    # Compute the set of all values v in a, given nan
-    set_a = set('nan' if isinstance(x, float) and math.isnan(x) 
-                else x for x in a) if nan else set(a)
+    # If nan is True, replace all nan values in a by the string "__nan__"
+    if nan:
+        a = ['__nan__' if isinstance(x, float) and math.isnan(x) else x for x in a]
     
     # Compute the sum of all values v in a
     sum_v = 0
-    for v in set_a:
-        Ex_a_v = _Ex_a_v_(Ex, a, v, nan)
+    for v in set(a):
+        Ex_a_v = [x for x, t in zip(Ex, a) if t == v]
         sum_v += (len(Ex_a_v) / len(Ex)) *\
                  (entropy(list(Counter(Ex_a_v).values())))
 
@@ -117,14 +79,14 @@ def intrinsic_value(Ex, a, nan=True):
     if len(Ex) != len(a):
         raise ValueError("Ex and a must be of the same size.")
 
-    # Compute the set of all values v in a, given nan
-    set_a = set('nan' if isinstance(x, float) and math.isnan(x) 
-                else x for x in a) if nan else set(a)
+    # If nan is True, replace all nan values in a by the string "__nan__"
+    if nan:
+        a = ['__nan__' if isinstance(x, float) and math.isnan(x) else x for x in a]
                 
     # Compute the sum of all values v in a
     sum_v = 0
-    for v in set_a:
-        Ex_a_v = _Ex_a_v_(Ex, a, v, nan)
+    for v in set(a):
+        Ex_a_v = [x for x, t in zip(Ex, a) if t == v]
         sum_v += (len(Ex_a_v) / len(Ex)) * math.log(len(Ex_a_v) / len(Ex), 2)
 
     # Return result
